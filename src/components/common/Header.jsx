a9bdb9064e12d1db822f75b7f6404079038443d7
justinait <justiiturraspe@gmail.com>
2023-11-08 14:31:48 -0300

    drawer

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,37 +2,153 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import CssBaseline from "@mui/material/CssBaseline";
 
-function Navbar() {
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import logo from '../../assets/logoEco.png'
+
+const drawerWidth = 280;
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+
+      <List>
+
+        <Link to='/'>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText
+                primary={"Proveedores"}
+                sx={{ color: "whitesmoke" }}
+              />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText
+              primary={"Publicaciones"}
+              sx={{ color: "whitesmoke" }}
+            />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText
+              primary={"Iniciá sesión"}
+              sx={{ color: "whitesmoke" }}
+            />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText
+              primary={"Registrate"}
+              sx={{ color: "whitesmoke" }}
+            />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <AppBar position="static" sx={(theme) => ({ bgcolor: "naranjas.medium" })}>
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          Nombre sitio
-        </Typography>
-        <Button color="inherit" href="/">
-          Inicio
-        </Button>
-        <Button color="inherit" href="/dashboard">
-          Dashboard
-        </Button>
-        <Button color="inherit" href="/layout">
-          Layout
-        </Button>
-        <Button color="inherit" href="/publicaciones">
-          Publicaciones
-        </Button>
-        <Button color="inherit" href="/proveedores">
-          Proveedores
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="white"
+        sx={{
+          width: "100%",
+          height: '10vh',
+          zIndex: '10',
+          
+        }}
+      >
+        <Toolbar
+          sx={{ gap: "20px", display: "flex", justifyContent: "space-around" }}
+        >
+          <IconButton
+            color="black"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon color="black" />
+          </IconButton>
+          <Link to="/" style={{ color: "whitesmoke" }}>
+            <img src={logo} alt="" />
+          </Link>
+          <Box
+            sx={{display: "flex", alignItems:'center', flexDirection:'column' }}
+          >
+            <AccountCircleOutlinedIcon color="black"/>
+            <div>Ingresá</div>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav" aria-label="mailbox folders">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          anchor={"left"}
+          onClose={handleDrawerToggle}
+          sx={{
+            display: { xs: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              height: '90vh',
+              top:'10vh',
+              backgroundColor: "#4E169D",
+              zIndex: '1'
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 4,
+          width: "100%",
+          minHeight: "100vh",
+          px: 2,
+        }}
+      >
+        <Toolbar />
+
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
 
