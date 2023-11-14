e545b44ee8ddbe16ffca6d9c6e82b5b0949a124a
Emiliano Barea <emilianobarea8@gmail.com>
2023-11-14 16:07:25 -0300

    nav details

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,80 +1,155 @@
 import React from "react";
+import { useState } from "react";
+
 import AppBar from "@mui/material/AppBar";
+import { Typography } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import CssBaseline from "@mui/material/CssBaseline";
 import CloseIcon from "@mui/icons-material/Close";
-import "./Header.css";
 
-import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logoEco.png";
 
-const drawerWidth = 280;
+const drawerWidth = 258;
 
 function Navbar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/ingresa";
+  const isRegisterPage = location.pathname === "/registrate";
+  const showAccountIcon = !isLoginPage && !isRegisterPage;
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
   const drawer = (
-    <div>
+    <Box>
       <List>
-        <Link to="/">
+        <Link to="/" onClick={handleDrawerToggle}>
           <ListItem disablePadding>
             <ListItemButton>
-              <Typography className="navbarItems">Inicio</Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Nunito",
+                  color: "#FAFAFA",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "700",
+                  lineHeight: "20px",
+                  margin: "2% 0",
+                }}
+              >
+                Inicio
+              </Typography>
             </ListItemButton>
           </ListItem>
         </Link>
 
-        <Link to="/">
+        <Link to="/" onClick={handleDrawerToggle}>
           <ListItem disablePadding>
             <ListItemButton>
-              <Typography className="navbarItems">Proveedores</Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Nunito",
+                  color: "#FAFAFA",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "700",
+                  lineHeight: "20px",
+                  margin: "2% 0",
+                }}
+              >
+                Proveedores
+              </Typography>
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to="/" onClick={handleDrawerToggle}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <Typography
+                sx={{
+                  fontFamily: "Nunito",
+                  color: "#FAFAFA",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "700",
+                  lineHeight: "20px",
+                  margin: "2% 0",
+                }}
+              >
+                Publicaciones
+              </Typography>
             </ListItemButton>
           </ListItem>
         </Link>
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <Typography className="navbarItems">Publicaciones</Typography>
-          </ListItemButton>
-        </ListItem>
-
-        <Link to="/ingresa">
+        <Link to="/ingresa" onClick={handleDrawerToggle}>
           <ListItem disablePadding>
             <ListItemButton>
-              <Typography className="navbarItems">Iniciá sesión</Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Nunito",
+                  color: "#FAFAFA",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "700",
+                  lineHeight: "20px",
+                  margin: "2% 0",
+                }}
+              >
+                Iniciá sesión
+              </Typography>
             </ListItemButton>
           </ListItem>
         </Link>
 
-        <Typography className="registerTextNavbar">
+        <Typography
+          sx={{
+            fontFamily: "Nunito",
+            color: "#FAFAFA",
+            fontSize: "18px",
+            fontStyle: "italic",
+            fontWeight: 400,
+            lineHeight: "22px",
+            width: "242px",
+            margin: "6% 6% 0",
+          }}
+        >
           {" "}
           ¿Querés formar parte de la Red de impacto ECO como Proveedor?{" "}
         </Typography>
-        <Link to="/registrate">
+        <Link to="/registrate" onClick={handleDrawerToggle}>
           <ListItem disablePadding>
             <ListItemButton>
-              <Typography className="navbarItems">Registrate</Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Nunito",
+                  color: "#FAFAFA",
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  lineHeight: "20px",
+                  margin: "2% 0",
+                }}
+              >
+                Registrate
+              </Typography>
             </ListItemButton>
           </ListItem>
         </Link>
       </List>
-    </div>
+    </Box>
   );
 
   const container =
@@ -104,19 +179,23 @@ function Navbar(props) {
           <Link to="/" style={{ height: "56px" }}>
             <img src={logo} alt="" />
           </Link>
-          <Link to="/ingresa">
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-                height: "40px",
-              }}
-            >
-              <AccountCircleOutlinedIcon color="black" />
-              <Box sx={{ height: "16px", color: "#000" }}>Ingresá</Box>
-            </Box>
-          </Link>
+          {showAccountIcon ? (
+            <Link to="/ingresa">
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  height: "40px",
+                }}
+              >
+                <AccountCircleOutlinedIcon color="black" />
+                <Box sx={{ height: "16px", color: "#000" }}>Ingresá</Box>
+              </Box>
+            </Link>
+          ) : (
+            <Box sx={{ width: "54px" }}></Box>
+          )}
         </Toolbar>
       </AppBar>
       <Box component="nav" aria-label="mailbox folders">
@@ -141,16 +220,6 @@ function Navbar(props) {
           {drawer}
         </Drawer>
       </Box>
-      {/* <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 4,
-          width: "100%",
-          minHeight: "20vh",
-          px: 2,
-        }}
-      ></Box> */}
     </Box>
   );
 }
