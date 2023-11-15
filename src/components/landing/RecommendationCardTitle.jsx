853f5ebf10d05a88663821e588c3e6709917a8cc
Emilio Pino <pinoen@yahoo.com.ar>
2023-11-15 11:43:42 -0300

    create component

---
+++ src/components/landing/RecommendationCardTitle.jsx
@@ -0,0 +1,42 @@
+import { Box, Typography } from '@mui/material'
+
+const subTitleStyle = {
+  color: "black.main",
+  fontFamily: "Nunito",
+  fontSize: "16px",
+  fontStyle: "normal",
+  fontWeight: "600",
+  lineHeight: "25px",
+}
+
+const titleStyle = {
+  color: "black.main",
+  fontFamily: "Nunito",
+  fontSize: "22px",
+  fontStyle: "normal",
+  fontWeight: "700",
+  lineHeight: "25px",
+}
+
+const boxStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  paddingLeft: "16px",
+  width: "100%",
+}
+const RecommendationCardTitle = () => {
+  return (
+    <Box sx={boxStyle}>
+      <Typography variant="h5" sx={subTitleStyle}>
+        Recomendaciones locales para vos
+      </Typography>
+      <Typography variant="h3" sx={titleStyle}>
+        Proveedores cerca tuyo
+      </Typography>
+    </Box>
+  )
+}
+
+export default RecommendationCardTitle
