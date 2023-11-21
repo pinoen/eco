a6fd8b2b90a12d9ec430d10f82b081735eb8cf38
Emilio Pino <pinoen@yahoo.com.ar>
2023-11-21 20:45:38 -0300

    add category blocks

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -7,7 +7,8 @@ import { useEffect, useState } from "react";
 import { getSuppliers } from "../../services/api";
 import SupplierCard from "../common/SupplierCard";
 import vector from "../../assets/img/Vector1.png";
-import RecommendationCardTitle from "./RecommendationCardTitle";
+import SectionTitle from "./SectionTitle";
+import CategoryBlock from "../common/CategoryBlock";
 
 const boxStyle = {
   py: 5,
@@ -17,12 +18,20 @@ const boxStyle = {
   alignItems: "center",
 };
 
-const gridStyle = {
+const gridCardsStyle = {
   background: `url(${vector})`,
   backgroundSize: "cover",
   backgroundPosition: "center",
   padding: "24px 17px 8px 17px",
+  marginBottom: '40px'
 };
+
+const gridCategoriesStyle = {
+  gap: "24px",
+  marginBottom: '40px',
+  padding: "24px 16px 32px 16px",
+  justifyContent: "center",
+}
 
 function LandingPage() {
   const [suppliers, setSuppliers] = useState([]);
@@ -42,14 +51,24 @@ function LandingPage() {
         <CTAButton>Registrate</CTAButton>
 
         {/* Tarjetas de Información */}
-        <RecommendationCardTitle />
-        <Grid container spacing={3} sx={gridStyle}>
-          {suppliers.map((item) => (
+        <SectionTitle title='Recomendaciones locales para vos' subtitle='Proveedores cerca tuyo' />
+        <Grid container spacing={3} sx={gridCardsStyle}>
+          {suppliers.slice(0, 4).map((item) => (
             <Grid item xs={6} sm={3} key={item.id}>
               <SupplierCard item={item} />
             </Grid>
           ))}
         </Grid>
+
+        <SectionTitle title='Red de Proveedores ECO' subtitle='Categorías' />
+
+        {/* Categorías */}
+        <Grid container spacing={3} sx={gridCategoriesStyle}>
+          {suppliers.map((item) => (
+            <CategoryBlock key={item.id} icon={item.categoryIcon} categoryName={item.category} />
+          ))}
+        </Grid>
+
       </Box>
     </Container>
   );
