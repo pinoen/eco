6b4d301427ef8fd5c24d5e36bc7aef621cafdfcc
pinoen <pinoen@yahoo.com.ar>
2023-11-22 21:20:02 -0300

    add page prop so to render different according to the route where the component is going to be used

--- src/components/common/CategoryBlock.jsx
+++ src/components/common/CategoryBlock.jsx
@@ -2,36 +2,36 @@
 import { Box, CardMedia, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom";
 
-const CategoryBlockStyle = {
-  display: "flex",
-  width: "152px",
-  height: "64px",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "10px",
-  borderRadius: "16px",
-  backgroundColor: "grey.main",
-  padding: "8px",
-  '&:hover': {
-    cursor: "pointer",
-  }
-}
-
-const TypographyStyle = {
-  color: "black.main",
-  fontFamily: "Nunito",
-  fontSize: "15px",
-  fontStyle: "normal",
-  fontWeight: 700,
-  lineHeight: "25px",
-  textDecoration: "underline",
-  textUnderlineOffset: "70%",
-  textDecorationColor: "#4E169D",
-  textDecorationThickness: "2px",
-}
-
-const CategoryBlock = ({ categoryName, icon }) => {
+const CategoryBlock = ({ categoryName, icon, page }) => {
   const navigate = useNavigate();
+
+  const CategoryBlockStyle = {
+    display: "flex",
+    width: `${page === "landing" ? "152px" : "328px"}`,
+    height: `${page === "landing" ? "64px" : "72px"}`,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
+    borderRadius: "16px",
+    backgroundColor: "grey.main",
+    padding: "8px",
+    '&:hover': {
+      cursor: "pointer",
+    }
+  }
+
+  const TypographyStyle = {
+    color: "black.main",
+    fontFamily: "Nunito",
+    fontSize: "15px",
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: "25px",
+    textDecoration: `${page === "landing" ? "underline" : "none"}`,
+    textUnderlineOffset: "70%",
+    textDecorationColor: "#4E169D",
+    textDecorationThickness: "2px",
+  }
 
   return (
     <Box sx={CategoryBlockStyle} onClick={() => navigate(`/proveedores/${categoryName}`)}>
@@ -44,6 +44,7 @@ const CategoryBlock = ({ categoryName, icon }) => {
           border: '1px solid black',
           borderRadius: "100%",
           padding: "3px",
+          width: '40px',
         }}
       />
       <Typography sx={TypographyStyle}>{categoryName}</Typography>
