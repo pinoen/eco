0b635d12c9c0a48e3bc604e2b501e995c9fde870
pinoen <pinoen@yahoo.com.ar>
2023-11-27 20:49:26 -0300

    change search bar location into the common folder

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 import { SearchRounded } from "@mui/icons-material"
 import { Autocomplete, Box, InputAdornment, TextField } from "@mui/material"
 import useSuppliers from "../../utilities/suppliers"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const boxStyle = {
   mt: 5,
@@ -10,11 +12,12 @@ const boxStyle = {
   height: 56,
   color: "black.main",
 }
-export const SearchBar = () => {
+export const SearchBar = ({ querySearch }) => {
   const suppliers = useSuppliers()
-  const suppliersList = suppliers.map((item) => item.name.toLowerCase())
+  const suppliersList = suppliers.map((item) => item.name)
+  const navigate = useNavigate()
 
-  const [value, setValue] = useState(null)
+  const [value, setValue] = useState(querySearch || null)
   const [inputValue, setInputValue] = useState("")
 
   return (
@@ -28,6 +31,12 @@ export const SearchBar = () => {
         onInputChange={(event, newInputValue) => {
           setInputValue(newInputValue);
         }}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            navigate(`/search/${inputValue}`)
+          }
+        }}
+
         disablePortal
         options={suppliersList}
         renderInput={(params) => <TextField
