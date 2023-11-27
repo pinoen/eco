d3049a0f756aeae6eb34879286f39de57e00e609
pinoen <pinoen@yahoo.com.ar>
2023-11-27 17:36:30 -0300

    add autocomplete material ui component to render available suppliers

--- src/components/landing/SearchBar.jsx
+++ src/components/landing/SearchBar.jsx
@@ -1,32 +1,54 @@
 import { SearchRounded } from "@mui/icons-material"
-import { Box, InputAdornment, TextField } from "@mui/material"
+import { Autocomplete, Box, InputAdornment, TextField } from "@mui/material"
+import useSuppliers from "../../utilities/suppliers"
+import { useState } from "react"
 
-
+const boxStyle = {
+  mt: 5,
+  mb: 5,
+  width: 328,
+  height: 56,
+  color: "black.main",
+}
 export const SearchBar = () => {
+  const suppliers = useSuppliers()
+  const suppliersList = suppliers.map((item) => item.name.toLowerCase())
+
+  const [value, setValue] = useState(null)
+  const [inputValue, setInputValue] = useState("")
+
   return (
-    <Box sx={{mt:5,mb:5, width:328,height:56,color:"#222"}}>
-      <TextField
-      
-      id="Buscador"
-      placeholder="Buscar Proveedores"
-      InputProps={{
-        startAdornment:(
-
-          <InputAdornment position="start">
-          <SearchRounded/>
-          </InputAdornment>
-        ),
-        style:{
-          borderRadius:"100px"
-          ,backgroundColor:"white"
-        },
-      }}
-      variant="outlined"
-      fullWidth
-      
+    <Box sx={boxStyle}>
+      <Autocomplete
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        disablePortal
+        options={suppliersList}
+        renderInput={(params) => <TextField
+          {...params}
+          placeholder="Buscar Proveedores"
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchRounded />
+              </InputAdornment>
+            ),
+            style: {
+              borderRadius: "100px",
+              backgroundColor: "white"
+            },
+          }}
+          variant="outlined"
+          fullWidth
+        />}
       />
-
-      
     </Box>
   )
 }
