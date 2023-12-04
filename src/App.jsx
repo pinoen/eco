13c5319f2fe1e6090ecbf99525c37a7c24b11d84
pinoen <pinoen@yahoo.com.ar>
2023-12-04 18:36:47 -0300

    add add-product route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Profile from "./components/profile/Profile";
 import Proveedores from "./components/proveedores/Proveedores";
 import Proveedor from "./components/proveedores/Proveedor";
 import SearchResult from "./components/search/SearchResult";
+import AddProduct from "./components/profile/AddProduct";
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
               {/* vista para registrate */}
               <Route path="/registrate" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/profile/add-product" element={<AddProduct />} />
               {/* Agregar rutas necesarias */}
             </Routes>
             {/* <Footer /> */}
