99f07172086c392b183a329ac38bc0b4d0bb628f
pinoen <pinoen@yahoo.com.ar>
2023-12-04 22:30:48 -0300

    add a padding bottom condition if an icon prop is passed

--- src/components/common/CTAButton.jsx
+++ src/components/common/CTAButton.jsx
@@ -3,40 +3,38 @@ import { Box, Button } from '@mui/material'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-const buttonStyle = {
-  width: 'auto',
-  height: '40px',
-  padding: '10px 24px',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '10px',
-  borderRadius: '100px',
-  backgroundColor: 'primary.main',
-  color: 'white.main',
-  fontFamily: 'Nunito',
-  fontSize: '16px',
-  fontStyle: 'normal',
-  fontWeight: '700',
-  lineHeight: '20px',
-  textTransform: 'capitalize',
-  '&:hover': {
-    backgroundColor: '#6100EA',
-  },
-}
-
-const boxStyle = {
-  backgroundColor: 'white.main',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '16px',
-  paddingBottom: '48px',
-  gap: '8px',
-  width: 'auto',
-}
-
 const CTAButton = ({ route, children, icon }) => {
+  const buttonStyle = {
+    width: 'auto',
+    height: '40px',
+    padding: '10px 24px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '10px',
+    borderRadius: '100px',
+    backgroundColor: 'primary.main',
+    color: 'white.main',
+    fontFamily: 'Nunito',
+    fontSize: '16px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '20px',
+    textTransform: 'capitalize',
+    '&:hover': {
+      backgroundColor: '#6100EA',
+    },
+  }
+
+  const boxStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '16px',
+    paddingBottom: icon ? '4px' : '48px',
+    gap: '8px',
+    width: 'auto',
+  }
   return (
     <Box sx={boxStyle}>
       <Link to={route}><Button startIcon={icon} sx={buttonStyle}>{children}</Button></Link>
