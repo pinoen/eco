784a2db2b4e13c12408ad0eda78e58f92194187a
pinoen <pinoen@yahoo.com.ar>
2024-01-04 10:16:29 -0300

    add style to button and fix error showing when validating input

--- src/components/admin/Feedback.jsx
+++ src/components/admin/Feedback.jsx
@@ -2,35 +2,63 @@ import { Button, TextField } from "@mui/material"
 import { useFormik } from "formik"
 import * as Yup from "yup";
 
+const buttonStyle = {
+  width: '328px',
+  height: '40px',
+  padding: '10px 24px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px',
+  borderRadius: '100px',
+  backgroundColor: 'primary.main',
+  color: 'white.main',
+  fontFamily: 'Nunito',
+  fontSize: '16px',
+  fontStyle: 'normal',
+  fontWeight: '700',
+  lineHeight: '20px',
+  textTransform: 'capitalize',
+  '&:hover': {
+    backgroundColor: '#6100EA',
+  },
+}
+
+const formStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '24px',
+  gap: '16px',
+}
 
 const Feedback = () => {
-
   const initialFeedback = {
     feedback: "",
   }
 
   const { handleSubmit, handleChange, values, errors } = useFormik({
-
     initialValues: initialFeedback,
     validateOnChange: false,
     validationSchema: Yup.object({
       feedback: Yup.string()
+        .min(10, "La descripción debe tener al menos 10 caracteres")
         .max(300, "Máximo 300 caracteres")
         .required("Devolución al Proveedor (Obligatorio)"),
     }),
     onSubmit: (values) => {
-      console.log(values)
+      console.log(values.feedback)
     }
   })
 
   return (
-    <form onSubmit={handleSubmit} style={{ paddingTop: "32px" }}>
+    <form onSubmit={handleSubmit} style={formStyle}>
       <TextField
         type="text"
         label="Devolución al Proveedor (Obligatorio)"
-        error={errors.description ? true : false}
+        error={errors.feedback ? true : false}
         helperText={
-          errors.description ? errors.description : "Máximo 300 caracteres"
+          errors.feedback ? errors.feedback : "Máximo 300 caracteres"
         }
         multiline
         rows={6}
@@ -40,7 +68,7 @@ const Feedback = () => {
         fullWidth
       />
 
-      <Button type="submit">Enviar</Button>
+      <Button sx={buttonStyle} type="submit">Enviar</Button>
     </form>
   )
 }
