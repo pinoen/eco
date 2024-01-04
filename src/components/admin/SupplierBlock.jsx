9b6ca8bddeb6e8715990af95bbf050f59b167c08
pinoen <pinoen@yahoo.com.ar>
2024-01-04 11:29:49 -0300

    add circle with a color according to the shown page

--- src/components/admin/SupplierBlock.jsx
+++ src/components/admin/SupplierBlock.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
+import Brightness1Icon from '@mui/icons-material/Brightness1';
 import { Box, Divider, Typography } from "@mui/material"
 
 const boxStyle = {
   display: "flex",
   width: "328px",
-  height: "72px",
+  height: "92px",
   padding: "8px 8px 8px 16px",
   alignItems: "center",
   justifyContent: "space-between",
@@ -15,6 +15,9 @@ const boxStyle = {
 }
 
 const textContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   width: "290px",
   height: "56px",
 }
@@ -27,6 +30,8 @@ const titleStyle = {
   fontWeight: 700,
   lineHeight: "24px",
   marginBottom: "4px",
+  display: "flex",
+  alignItems: "center",
 }
 
 
@@ -40,11 +45,17 @@ const categoryStyle = {
   marginTop: "4px",
 }
 
-const SupplierBlock = ({ setShowSupplier }) => {
+const SupplierBlock = ({ setShowSupplier, statusPage }) => {
   return (
     <Box sx={boxStyle}>
       <Box sx={textContainerStyle} onClick={() => setShowSupplier(pre => !pre)}>
-        <Typography sx={titleStyle}>Lavanda</Typography>
+        <Typography sx={titleStyle}>{
+          statusPage !== "Nuevos Perfiles" &&
+          <Brightness1Icon
+            fontSize='large'
+            sx={{ color: statusPage === "Aprobados" ? "#1D9129" : statusPage === "En revisión" ? "#B86B11" : statusPage === "Denegados" ? "#B91C1C" : "none", paddingRight: "10px" }}
+          />
+        }Lavanda</Typography>
         <Divider sx={{ width: "75%", borderColor: "#00A364", borderTopWidth: "2px" }} />
         <Typography sx={categoryStyle}>Cosmética natural</Typography>
       </Box>
