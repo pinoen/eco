e5e5f73d7451eb68a40062df2cfac889a6098d97
pinoen <pinoen@yahoo.com.ar>
2024-01-05 11:40:59 -0300

    remove supplier block and conditions to been rendered

--- src/components/admin/NewProfiles.jsx
+++ src/components/admin/NewProfiles.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { useStatusContext } from "../../context/StatusContext"
-import SupplierBlock from "./SupplierBlock"
 import Brightness1Icon from '@mui/icons-material/Brightness1';
 import { Box, Typography } from "@mui/material";
 import DropDownStatus from "./DropDownStatus";
@@ -17,32 +16,22 @@ const statusStyle = {
   color: "black.main",
 }
 
-const NewProfiles = ({ showSupplier, setShowSupplier }) => {
-  const { status, statusPage } = useStatusContext()
+const NewProfiles = ({ showSupplier }) => {
+  const { status } = useStatusContext()
 
   return (
     <>
-      {showSupplier ? null : <SupplierBlock setShowSupplier={setShowSupplier} statusPage={statusPage} />}
+      <Box sx={{ display: "flex", alignItems: "center", paddingBottom: "24px", paddingTop: "24px" }}>
+        {status !== "" && <><Brightness1Icon
+          fontSize='large'
+          sx={{ color: status === "Aprobado" ? "#1D9129" : status === "En revisión" ? "#B86B11" : "#B91C1C", paddingRight: "10px" }}
+        />
+          <Typography sx={statusStyle}>{status}</Typography></>}
+      </Box>
 
-      {showSupplier ?
-
-        <>
-          {status ?
-            <Box sx={{ display: "flex", alignItems: "center", paddingBottom: "24px" }}>
-              <Brightness1Icon
-                fontSize='large'
-                sx={{ color: status === "Aprobado" ? "#1D9129" : status === "En revisión" ? "#B86B11" : "#B91C1C", paddingRight: "10px" }}
-              />
-              <Typography sx={statusStyle}>{status}</Typography>
-            </Box>
-            :
-            null}
-          <DropDownStatus />
-          {status === "Denegado" || status === "En revisión" ? <Feedback /> : null}
-          <AddProduct showSupplier={showSupplier} />
-        </> :
-
-        null}
+      <DropDownStatus />
+      {status === "Denegado" || status === "En revisión" ? <Feedback /> : null}
+      <AddProduct showSupplier={showSupplier} />
     </>
   )
 }
