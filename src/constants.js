be93da73e5a72b44aabf55d15737ab8b7f27e356
Guillermo Bergesio <bergesiog1@gmail.com>
2024-01-05 14:16:58 -0300

    Test constantes para deploy front 2

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@
 // export const googleClientID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 // export const jwtSecretKey = import.meta.env.VITE_JWT_SECRET_KEY;
 
-export const apiUrl = "http://localhost:8080/api";
+export const apiUrl = "http://t-ecos3.qi.local:31645/api";
 export const googleClientID = "672507101516-tmlt4qc29tpb6dv4vrohdbolis8bnlih.apps.googleusercontent.com";
 export const jwtSecretKey = 'HJkLpQsYuGvXwBzNrMtFcEiOaR'
